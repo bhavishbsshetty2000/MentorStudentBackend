d382e9c03d9bf8b4c88cedc9d5de2a62f387c259
bhavishbsshetty2000 <bhavishbsshetty2000@gmail.com>
2022-02-24 21:03:15 +0530

    index updated

--- index.js
+++ index.js
@@ -219,8 +219,7 @@ app.post("/assign-students-to-mentor", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 5050;
-
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server started in port ${port}`);
+  console.log(`Server Started in port ${port}`);
 });
